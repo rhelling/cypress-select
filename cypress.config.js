06cf20ebb488b30d09b3dc744dae9a7b9dee6b40
Robbert Helling <robbert@codebron.nl>
2023-08-24 13:59:17 +0200

    finalize example test

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  includeShadowDom: true,
   e2e: {
     setupNodeEvents(on, config) {},
   },
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,8 @@
 /// <reference types="cypress" />
 describe('page', () => {
   it('works', () => {
-    cy.visit('https://example.cypress.io')
+    cy.visit('https://heap.nl/~robbert/select/dist/');
+    cy.get('select').select('a_option');
+    cy.get('[data-ta="selected"]').should('have.text', 'a_option');
   })
 })
